18b3c5b129d3958bba77be80d2545387cd69e4cd
testting <phuchana.wirancha@swu.ac.th>
2026-04-23 19:03:53 +0700

    initial setup: connect database

--- list.php
+++ list.php
@@ -1,12 +1,19 @@
 <?php
-// ===== Database Config =====
 $host = "127.0.0.1";
 $user = "root";
 $pass = "16112548";
 $db   = "student_db";
 
-// ===== Connect Database =====
 $conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully";
+
+$conn->close();
+?>
 
 // Check connection
 if ($conn->connect_error) {
